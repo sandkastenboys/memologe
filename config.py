411a5696f13c72c948c7d0d39a1919e7c344b229
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-02-24 15:09:44 +0100

    root pw in mysql can't be empty

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ config = {
     "db_host": "127.0.0.1",
     "db_port": 3307,
     "db_user": "root",
-    "db_pw": "",
+    "db_pw": "memes_are_the_best",
     "tele_token": "778745612:AAHFnKvwmRTvS2wDtBlAvbCHXmHgjum9hYM",  # Telegram Bot token
     "max_post": 10
 }
